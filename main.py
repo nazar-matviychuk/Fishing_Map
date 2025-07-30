bd0ac1f2e9d877a22c0ae122e1648264d524333f
nazar-matviychuk <161215898+nazar-matviychuk@users.noreply.github.com>
2025-07-30 21:24:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import sqlite3, os, uuid
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
 
 app = FastAPI()
 os.makedirs("images", exist_ok=True)
